f5ee0d3a01d5470db0827a57d73cc1b6ddc9e7b2
AustinMaddox <austin@maddoxbox.com>
2016-05-22 17:51:40 -0600

    Configure deployment to separate clusters (staging|production)

--- AWS/ECS/clusters/web-staging-cluster/user-data.sh
+++ AWS/ECS/clusters/web-staging-cluster/user-data.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # This script is the "User data" or sometimes referred to as the "Cloud init script" that is run during bootup of a new EC2 server. This script is copied into the user data upon creating a launch configuration for auto scaling.
 #
 # @author: "Austin Maddox" <austin@maddoxbox.com>
@@ -7,7 +6,7 @@
 # You can specify user data to configure an instance or run a configuration script during launch. If you launch more than one instance at a time, the user data is available to all the instances in that reservation.
 
 # Specify which ECS cluster to associate this instance with.
-echo ECS_CLUSTER=default >> /etc/ecs/ecs.config
+echo ECS_CLUSTER=web-staging-cluster >> /etc/ecs/ecs.config
 
 cd /tmp
 
@@ -21,7 +20,7 @@ python27 get-pip.py
 /usr/local/bin/pip install --upgrade awscli
 
 # Grab all configuration files from S3.
-su - ec2-user -c "aws s3 cp s3://mmginc-web-settings /home/ec2-user/mmginc-web-settings --recursive"
+su - ec2-user -c "aws s3 cp s3://mmginc-web-settings/staging /home/ec2-user/mmginc-web-settings/staging --recursive"
 
 # Install the Simple Systems Manager (SSM) for the Amazon EC2 Run Command to manage EC2 instances remotely.
 curl https://amazon-ssm-us-west-2.s3.amazonaws.com/latest/linux_amd64/amazon-ssm-agent.rpm -o amazon-ssm-agent.rpm
