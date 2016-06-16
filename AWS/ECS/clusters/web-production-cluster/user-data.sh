988179838103cb7f63fabe68dabd42f70a45cb85
AustinMaddox <austin@maddoxbox.com>
2016-06-15 21:38:20 -0600

    Docker container and image cleanup

--- AWS/ECS/clusters/web-production-cluster/user-data.sh
+++ AWS/ECS/clusters/web-production-cluster/user-data.sh
@@ -10,6 +10,11 @@ echo ECS_CLUSTER=web-production-cluster >> /etc/ecs/ecs.config
 
 cd /tmp
 
+# Adjust the frequency of the task that cleans up stopped containers. (Default: 3h)
+cat /etc/ecs/ecs.config | grep -v 'ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION' > /tmp/ecs.config
+echo "ECS_ENGINE_TASK_CLEANUP_WAIT_DURATION=10m" >> /tmp/ecs.config
+mv -f /tmp/ecs.config /etc/ecs/
+
 # Download pip.
 curl -O https://bootstrap.pypa.io/get-pip.py
 
@@ -25,3 +30,6 @@ su - ec2-user -c "aws s3 cp s3://mmginc-web-settings/production /home/ec2-user/m
 # Install the Simple Systems Manager (SSM) for the Amazon EC2 Run Command to manage EC2 instances remotely.
 curl https://amazon-ssm-us-west-2.s3.amazonaws.com/latest/linux_amd64/amazon-ssm-agent.rpm -o amazon-ssm-agent.rpm
 yum install -y amazon-ssm-agent.rpm
+
+# Pull the latest Spotify garbage collection image so we can run it on deployment later.
+docker pull spotify/docker-gc:latest
